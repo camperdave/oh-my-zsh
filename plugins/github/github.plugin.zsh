136a5565388b856b1f8754e7525bfde5483546cd
Dave Lee <camperdave614@gmail.com>
2011-05-08 19:59:55 -0500

    Adds a cool function to the github plugin. Without fetching any remotes, this queries GitHub to see if the specified remote and branch have any changes that would need to be pulled to the local branch. Parameters: gh-has-update [targetRemote=origin] [targetBranch=master] [localBranch=$(current_branch)] [myGithubUsername=$(git config --get github.user)] Note: this uses github's compare feature, so if you have changes locally that are not yet pushed to github, it will NOT return the correct data.

--- plugins/github/github.plugin.zsh
+++ plugins/github/github.plugin.zsh
@@ -5,3 +5,69 @@ if [ "$commands[(I)hub]" ]; then
     function git(){hub "$@"}
 fi
 
+gh-has-update() {
+    
+    if [ -z $4 ]; then
+        myUsername=$(git config --get "github.user")
+    else
+        myUsername=$4
+    fi
+
+    if [ -z $3 ]; then
+        myBranch=$(current_branch)
+    else
+        myBranch=$3
+    fi
+    
+    if [ -z $1 ]; then
+        targetRemote=origin
+    else
+        targetRemote=$1
+    fi
+    
+    if [ -z $2 ]; then
+        theirBranch=master
+    else
+        theirBranch=$2
+    fi
+    
+    repoName=${${(%):-%1d}#.}
+
+    oldIFS=$IFS
+    
+    IFS=$'\n'
+    
+    set -A remotes $(git remote -v)
+    
+    
+    IFS=oldIFS
+    
+    frontSub="$targetRemote*github.com/"
+    backSub="$repoName.git*\(fetch\)"
+    backSub="/$backSub"
+    
+    for i in $remotes; do;
+        name=${i#${~frontSub}}
+        if [ "$i" != "$name" ]; then
+            stepOne=$name
+            name=${name/${~backSub}} 
+            if [ "$stepOne" != "$name" ]; then
+                getURL="https://github.com/$myUsername/$repoName/compare/$myBranch...$name:$theirBranch"
+                netReq=$(wget --no-check-certificate --quiet $getURL -O -)
+                netS=$?
+                if [ $netS -eq 0 ]; then
+                    echo $netReq | grep "is up to date" -q
+                    if [ $? -eq 0 ]; then
+                        return 1
+                    else
+                        return 0
+                    fi
+                else
+                    echo "Bad URL: $getURL" >&2
+                fi
+            fi 
+        fi
+    done;
+    echo "Was not able to find the requested remote repository..." >&2
+    return 1
+}
